79b7a3cb6b294b7beb812020d604c037d25b321b
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-05-20 17:54:52 -0300

    feat: connection db postgres with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_HOST_DATABASE,
+      password: process.env.DB_HOST_PASSWORD,
+      username: process.env.DB_HOST_USER,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_HOST_PORT),
+      synchronize: true,
+      entities: [`${__dirname}/**/*.entity{*.ts,*.js}`],
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
